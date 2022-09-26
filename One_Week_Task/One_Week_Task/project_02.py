2207cc5fe4b6b516d064b2460b8d42ed4e2c86aa
kbm1933 <wjsalsgh456@gmail.com>
2022-09-26 20:20:18 +0900

    장고 실습 코드 모음

---
+++ One_Week_Task/One_Week_Task/project_02.py
@@ -0,0 +1,14 @@
+class report:
+    id = ''
+    title = ''
+    author = ''
+    content = ''
+
+note = report()
+
+note.id = 'kkk'
+note.tile = 'python'
+note.author = 'im'
+note.content = 'abcdefg'
+
+print(note.id)
